813e8aac79bac203ab7bf03ad67d610de09256f8
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2022-07-27 15:32:48 +0900

    완성

---
+++ src/main/java/com/project/springproject1/Main.java
@@ -0,0 +1,11 @@
+package com.project.springproject1;
+
+import org.springframework.boot.SpringApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProject1Application.class, args);
+    }
+
+}
+
